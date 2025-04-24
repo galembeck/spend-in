fcde9305fc8190aa08a671761d90b3f24f161848
Pedro Galembeck <galembeckpedro@gmail.com>
2025-04-24 14:04:58 -0300

    refactor: update metadata config

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ export const spendInConfig = {
     default: "Spend.In | All your business expenses in one place",
     template: `%s | Spend.In`,
   },
-  url: "https://spend-in.vercel.app",
+  url: "https://spend-in-g.vercel.app",
   description:
     "Your one-stop finnace empower platform. Manage all your business expenses with our supafast app.",
   locale: "en_US", // en_US, de_DE, fr_FR, es_ES, it_IT, pt_PT, ru_RU, zh_CN, ja_JP, ko_KR, pt_BR
