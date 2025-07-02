588aba302a2c6780ae53639e61e00f04428b0ebb
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2025-07-02 12:00:24 -0300

    refator: remove unused comments

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,13 +6,11 @@ import { TransactionHistory } from "@/components/(dashboard)/transaction-history
 export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-6">
-      {/* Top Row: Statistics + Right Column */}
       <div className="flex flex-col lg:flex-row gap-6">
-        {/* Left: Spending Statistics */}
         <div className="flex-1">
           <SpendingStatistics />
         </div>
-        {/* Right: Balance Card + Transaction History (+ Spend Category) */}
+
         <div className="w-full lg:w-1/3 flex flex-col gap-6">
           <BalanceCard />
           <TransactionHistory />
@@ -20,13 +18,11 @@ export default function DashboardPage() {
         </div>
       </div>
 
-      {/* Middle Row: Income/Expenses */}
       <div className="flex flex-col lg:flex-row gap-6">
-        {/* Left: Income/Expenses */}
         <div className="flex-1">
           <IncomeExpenses />
         </div>
-        {/* Right: (empty or SpendCategory if needed) */}
+
         <div className="w-full lg:w-1/3">{/* <SpendCategory /> */}</div>
       </div>
     </div>
